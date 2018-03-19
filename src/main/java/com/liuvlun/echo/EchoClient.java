4cbb2eb304e80ee82f5e90573e59bfd64f91d119
Hellon <liuqinglong_88@sina.com>
2018-03-19 21:03:59 +0800

    使用bootstarp工具，简化代码

--- src/main/java/com/liuvlun/echo/EchoClient.java
+++ src/main/java/com/liuvlun/echo/EchoClient.java
@@ -1,10 +1,7 @@
 package com.liuvlun.echo;
 
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import com.liuvlun.utils.BootStrapUtil;
+
 
 /**
  * Created by Administrator on 2018/3/17 0017.
@@ -16,31 +13,7 @@ public class EchoClient {
 
     public static void main(String[] args){
 
-        EventLoopGroup group = new NioEventLoopGroup();
-
-        Bootstrap bootstrap = new Bootstrap();
-
-
-        bootstrap.group(group)
-                    .channel(NioSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY,true)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new EchoClientHandler());
-                        }
-                    });
-
-
-        try {
-            ChannelFuture f =  bootstrap.connect(HOST,PORT).sync();
-
-            f.channel().closeFuture().sync();
-
-        } catch (InterruptedException e) {
-            group.shutdownGracefully();
-        }
-
-
+        BootStrapUtil.clientStart(HOST,PORT,new EchoClientHandler());
     }
 
 }
